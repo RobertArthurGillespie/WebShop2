56fc49e1bddaf2f6b7003ee3179770f9282f8cb6
Robert Gillespie <RobertArthurGillespie@outlook.com>
2019-03-08 12:47:42 -0600

    ProductCategory model, views and controller working

---
+++ WebShop2.DataAccess.InMemory/ProductCategoryRepository.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Caching;
+using System.Text;
+using System.Threading.Tasks;
+using WebShop2.Core.Models;
+
+namespace WebShop2.DataAccess.InMemory
+{
+    public class ProductCategoryRepository
+    {
+        public ObjectCache cache = MemoryCache.Default;
+        public List<ProductCategory> productCategories;
+
+        public ProductCategoryRepository()
+        {
+            if (productCategories == null)
+            {
+                productCategories = new List<ProductCategory>();
+            }
+
+            productCategories = cache["productCategories"] as List<ProductCategory>;
+        }
+
+        public void AddProductCategory(ProductCategory productCategory)
+        {
+            productCategories.Add(productCategory);
+        }
+
+        public void Commit()
+        {
+            cache["productCategories"] = productCategories;
+        }
+
+        public void Collection()
+        {
+            productCategories.AsQueryable();
+        }
+    }
+}
---
+++ WebShop2.WebUI/Controllers/ProductCategoryManagerController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebShop2.Core.Models;
+using WebShop2.DataAccess.InMemory;
+
+namespace WebShop2.WebUI.Controllers
+{
+    public class ProductCategoryManagerController : Controller
+    {
+        public ProductCategoryRepository context;
+        // GET: ProductManager
+        public ProductCategoryManagerController()
+        {
+            context = new ProductCategoryRepository();
+        }
+        public ActionResult Index()
+        {
+            if (context.productCategories == null)
+            {
+                context.productCategories = new List<ProductCategory>();
+            }
+
+            /*Product product = new Product();
+            product.Name = "Superman";
+            product.Category = "Toy";
+            product.Price = 1000;
+            context.AddProduct(product);*/
+            context.Commit();
+            context.productCategories.ToList<ProductCategory>();
+            context.Collection();
+            return View(context.productCategories);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+
+        }
+
+        [HttpPost]
+        public ActionResult Create(ProductCategory productCategory)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+            else
+            {
+                context.AddProductCategory(productCategory);
+                context.Commit();
+                return RedirectToAction("Index");
+            }
+
+        }
+
+        public ActionResult Edit(ProductCategory productCategory)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Edit(string id, ProductCategory productCategory)
+        {
+
+            ProductCategory productCategoryToEdit = context.productCategories.Find(p => p.ID == id);
+            
+            productCategoryToEdit.Category = productCategory.Category;
+            
+            context.Commit();
+            return RedirectToAction("Index");
+        }
+    }
+}
